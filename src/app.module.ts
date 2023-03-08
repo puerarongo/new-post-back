2e4028c8ce3f22ba7f442f9d73817fa265399534
puerarongo <akhardin96@gmail.com>
2023-03-08 19:03:24 +0200

    start
    - add env
    - add mongoose
    - add route places

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const DB_HOST = `mongodb+srv://NewPost:EEqspbpOMh7mRtF1@cluster0.6rke8.mongodb.net/db-post?retryWrites=true&w=majority`;
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    MongooseModule.forRoot(DB_HOST),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
